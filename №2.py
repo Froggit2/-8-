0d0c464b6b481c2e37c78d61af9269101a237c9b
Froggit2 <165292357+Froggit2@users.noreply.github.com>
2024-05-19 10:50:44 +0300

    Add files via upload

---
+++ №2.py
@@ -0,0 +1,11 @@
+class House:
+    def __init__(self):
+        self.numberOfFloors = 0
+    def setNewNumberOfFloors(self, floors):
+        numberOfFloors = floors
+        print(numberOfFloors)
+
+
+
+home = House()
+home.setNewNumberOfFloors(5)
